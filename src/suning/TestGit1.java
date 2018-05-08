00a9dc66ec99d79b81aadff19fa1c7581dc0fd59
Administrator <Administrator@USER-20180329QP>
2018-05-08 16:25:11 +0800

    提交

---
+++ src/suning/TestGit1.java
@@ -0,0 +1,23 @@
+package suning;
+/*
+ * Copyright (C), 2002-2018, 苏宁易购电子商务有限公司
+ * FileName: TestGit1.java
+ * Author:   18041949(闫昆朋)
+ * Date:     2018年5月8日 下午3:22:35
+ * Description: //描述当前类所属模块  
+ * History: //修改记录
+ * <author>      <time>      <version>    <desc>
+ * 修改人姓名             修改时间            版本号                  描述
+ */
+/**
+ * @author 18041949(闫昆朋):
+ * @version 创建时间：2018年5月8日 下午3:22:36
+ * 类说明
+ */
+public class TestGit1 {
+	
+	public static void main(String[] args) {
+		System.out.println("hahaha");
+	}
+
+}
